80509267f2f8e03ae9e7ef9b48d12ed7f6684d5b
nouhship88 <nouhship88@gmail.com>
2022-10-03 20:53:02 -0400

    Descreption

---
+++ calculator.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+
+class calculator {
+    public static void main(String[] args) {
+
+        int x=5;
+        int y=50;
+        double result=0;
+char expression=' ';
+        Scanner in = new Scanner(System.in);
+System.out.println("Enter first number");
+x=in.nextInt();
+
+System.out.println("Enter second number");
+y=in.nextInt();
+
+System.out.println("choose expression -*/+");
+expression=in.next().charAt(0);
+
+switch(expression) {
+    case '-':
+result=x-y;
+      break;
+    case '+':
+    result=x+y;
+    break;
+    case '*':
+    result=x*y;
+    break;
+    case '/':
+    result=x/y;
+    break;
+    default:
+      System.out.println("Wrong entrance");
+  }
+
+
+        System.out.println(result); // Display the string.
+
+        in.close();
+    }
+}
